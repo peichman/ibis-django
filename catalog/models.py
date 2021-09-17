6db0a11516d840f8c5711cc7b80a6be08b9afd51
Peter Eichman <peter@echodin.net>
2021-09-17 14:24:10 -0400

    Made book to author a many-to-many relationship.
    - Added a Person model
    - Added a catalog index view
    - Added UUID and ISBN fields to the Book model

--- catalog/models.py
+++ catalog/models.py
@@ -1,8 +1,25 @@
+from uuid import uuid4
+
 from django.db import models
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=256)
+    sort_name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
 
 
 class Book(models.Model):
     title = models.CharField(max_length=1024)
+    authors = models.ManyToManyField(Person)
+    isbn = models.CharField('ISBN', max_length=13)
+    uuid = models.UUIDField('UUID', default=uuid4)
 
     def __str__(self):
-        return self.title
+        author_names = ', '.join(str(p) for p in self.authors.all())
+        return f'{self.title}, by {author_names}'
+
+    def author_list(self):
+        return self.authors.all()
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,13 @@
-from django.http import HttpResponse
+from django.http import HttpRequest
+from django.shortcuts import render
+
+from .models import Book
 
 
-def index(request):
-    return HttpResponse('Hello world!')
+def index(request: HttpRequest):
+    if 'author' in request.GET:
+        booklist = Book.objects.filter(authors__name__in=[request.GET['author']])
+    else:
+        booklist = Book.objects.all()
+
+    return render(request, 'catalog/index.html', context={'books': booklist})
