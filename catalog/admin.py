6a0649ad24ee63e0da3423cbe4d5190c4e9fae46
Peter Eichman <peter@echodin.net>
2021-09-21 12:41:22 -0400

    Added autocomplete for book title and person name

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,18 +3,25 @@ from django.contrib import admin
 from .models import Book, Person, Authorship, Series, SeriesMembership
 
 
+class PersonAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
 class AuthorshipInline(admin.TabularInline):
     model = Authorship
     extra = 1
+    autocomplete_fields = ['person']
 
 
 class BookAdmin(admin.ModelAdmin):
     fields = ['title', 'isbn', 'uuid']
     inlines = [AuthorshipInline]
+    search_fields = ['title']
 
 
 class SeriesMembershipInline(admin.TabularInline):
     model = SeriesMembership
+    autocomplete_fields = ['book']
 
 
 class SeriesAdmin(admin.ModelAdmin):
@@ -24,6 +31,5 @@ class SeriesAdmin(admin.ModelAdmin):
 
 # Register your models here.
 admin.site.register(Book, BookAdmin)
-admin.site.register(Person)
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Series, SeriesAdmin)
-#admin.site.register(Authorship)
