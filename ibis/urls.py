d7826637e57b90aa562b1cd7fb28c3f74bd45ffe
Peter Eichman <peter@echodin.net>
2022-09-29 16:28:37 -0400

    Added a redirect from "/" to "/catalog".

--- ibis/urls.py
+++ ibis/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='index')),
     path('catalog/', include('catalog.urls')),
     path('admin/', admin.site.urls),
 ]
