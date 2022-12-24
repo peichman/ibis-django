1c9cb21edbb67f4b7889ef2681edc8f0b37ca7fd
Peter Eichman <peter@echodin.net>
2022-12-23 20:43:52 -0500

    Use django-environ to configure the app.
    Added psycopg2 to the dependencies for PostgreSQL support.

--- ibis/settings.py
+++ ibis/settings.py
@@ -12,20 +12,30 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+# set casting, default value for environment variables
+env = environ.Env(
+    ALLOWED_HOSTS=(list, ['localhost']),
+    DEBUG=(bool, False)
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@u)o*^b)$73$rl%mzsz@hm_*@zlx35ks9iev@l3c-2y&wdvgx3'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', 'grim.ath.cx', 'ibis.grim.ath.cx']
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 
 # Application definition
@@ -76,10 +86,7 @@ WSGI_APPLICATION = 'ibis.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(),
 }
 
 
